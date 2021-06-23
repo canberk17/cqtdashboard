3623dd1ff2fb9127f735c00bcf741a05309a18a9
Canberk <canberk697@gmail.com>
2021-06-23 08:57:24 -0700

    Covalent Dashboard

--- app.py
+++ app.py
@@ -264,7 +264,8 @@ def display(value,address):
 
                     ])
                 )
-        return lis
+            
+            return lis
 
 
             
@@ -276,4 +277,4 @@ def update_news_div(n):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
