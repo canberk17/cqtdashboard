0f11b8e2704aaf5afca2457f31b3ed7eb00e75a0
Canberk <canberk697@gmail.com>
2021-06-23 08:38:22 -0700

    Covalent Dashboard

--- app.py
+++ app.py
@@ -247,23 +247,23 @@ def display(value,address):
             data['timestamp']=set_time(data)
 
             lis.append( html.Div([
-             dcc.Graph(
-                    id='graph1',
-                    figure=fig1(data),
-                    style={'display': 'inline-block'
+                    dcc.Graph(
+                            id='graph1',
+                            figure=fig1(data),
+                            style={'display': 'inline-block'
 
-                 },
-             ),
-            dcc.Graph(
-                id='graph2',
-                figure=fig2(data),
-                style={'display': 'inline-block'
+                        },
+                    ),
+                    dcc.Graph(
+                        id='graph2',
+                        figure=fig2(data),
+                        style={'display': 'inline-block'
 
-                },
-            )
+                        },
+                    )
 
-            ])
-           )
+                    ])
+                )
         return lis
 
 
